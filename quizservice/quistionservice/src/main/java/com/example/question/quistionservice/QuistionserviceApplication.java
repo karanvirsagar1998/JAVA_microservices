591630652ea9e6d2a6e478f2142ac59bacced681
Karanvir Singh <karanvirsagar1998@gmail.com>
2024-09-29 19:51:26 +0000

    questionService

---
+++ quizservice/quistionservice/src/main/java/com/example/question/quistionservice/QuistionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.question.quistionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuistionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuistionserviceApplication.class, args);
+	}
+
+}
---
+++ quizservice/quistionservice/src/test/java/com/example/question/quistionservice/QuistionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.question.quistionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuistionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
